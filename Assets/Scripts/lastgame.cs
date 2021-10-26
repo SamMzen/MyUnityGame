dc94dabf085a5374fadc40cb39b0bdca5a402dc5
Samuel Marrache <samuel.marrache@lilalions.com>
2021-10-26 16:45:36 +0200

    finition mini jeu cookie sur le hub

--- Assets/Scripts/lastgame.cs
+++ Assets/Scripts/lastgame.cs
@@ -7,11 +7,26 @@ public class lastgame : MonoBehaviour
 {
     public GameObject cookie;
     public GameObject cookie2;
+    public GameObject code;
+    public GameObject cookiefin, cookiefin2;
 
     public void cookieactive()
     {
         cookie.SetActive(true);
         cookie2.SetActive(true);
+    }
+
+    public void cookietive()
+    {
+        cookiefin.SetActive(true);
+        cookiefin2.SetActive(true);
+        cookie.SetActive(false);
+        cookie2.SetActive(false);
+    }
+
+    public void cookieative()
+    {
+        code.SetActive(true);
     }
 
 }
