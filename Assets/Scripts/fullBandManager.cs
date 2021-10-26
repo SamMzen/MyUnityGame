df2a1af698ef420e152b6bb9302500a1f880f2ac
Samuel Marrache <samuel.marrache@lilalions.com>
2021-10-26 22:05:53 +0200

    début de l'apparition des erreurs lors du game over

--- Assets/Scripts/fullBandManager.cs
+++ Assets/Scripts/fullBandManager.cs
@@ -6,9 +6,22 @@ using UnityEngine.SceneManagement;
 public class fullBandManager : MonoBehaviour
 {
     public GameManagerE2 gameManagerE2;
+    public GameObject error, error1, error2, error3, error4, error5, error6, error7, error8, error9, error10;
+    public int err;
 
     void Start()
     {
+        error.SetActive(false);
+        error1.SetActive(false);
+        error2.SetActive(false);
+        error3.SetActive(false);
+        error4.SetActive(false);
+        error5.SetActive(false);
+        error6.SetActive(false);
+        error7.SetActive(false);
+        error8.SetActive(false);
+        error9.SetActive(false);
+        error10.SetActive(false);
         gameManagerE2 = GameObject.FindGameObjectWithTag("GameManagerE2").GetComponent<GameManagerE2>();
     }
 
@@ -28,9 +41,17 @@ public class fullBandManager : MonoBehaviour
                 else if (gameManagerE2.life <= 0 && gameManagerE2.isCollisionActive)
                 {
                     gameManagerE2.bandSpeed = 0;
-                    SceneManager.LoadScene("Ecran Défaite");
+                    StartCoroutine(ExampleCoroutine());
+                    //  error.SetActive(true);
+                    //   error1.SetActive(true);
+                    //    error2.SetActive(true);
+                    //     error3.SetActive(true);
+                    //      error4.SetActive(true);
+                    //       error5.SetActive(true);
 
-                    Cursor.SetCursor(gameManagerE2.cursorTexture2, gameManagerE2.hotSpot, gameManagerE2.cursorMode);
+                    //        SceneManager.LoadScene("Ecran Défaite");
+
+                    //     Cursor.SetCursor(gameManagerE2.cursorTexture2, gameManagerE2.hotSpot, gameManagerE2.cursorMode);
                 }
             }
 
@@ -47,4 +68,25 @@ public class fullBandManager : MonoBehaviour
         }
 
     }
+
+    IEnumerator ExampleCoroutine()
+    {
+        error.SetActive(true);
+        error1.SetActive(true);
+        error2.SetActive(true);
+        error3.SetActive(true);
+        error4.SetActive(true);
+        error5.SetActive(true);
+        error6.SetActive(true);
+        error7.SetActive(true);
+        error8.SetActive(true);
+        error9.SetActive(true);
+        error10.SetActive(true);
+        yield return new WaitForSeconds(5);
+
+        SceneManager.LoadScene("Ecran Défaite");
+
+        Cursor.SetCursor(gameManagerE2.cursorTexture2, gameManagerE2.hotSpot, gameManagerE2.cursorMode);
+    }
+
 }
