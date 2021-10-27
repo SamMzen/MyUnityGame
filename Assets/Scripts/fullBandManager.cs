dc2741f3209cec682b6c141f32be8141a5602253
Samuel Marrache <samuel.marrache@lilalions.com>
2021-10-27 18:47:33 +0200

    ajout effet des messages d'erreurs lorsque l'on perd

--- Assets/Scripts/fullBandManager.cs
+++ Assets/Scripts/fullBandManager.cs
@@ -6,22 +6,9 @@ using UnityEngine.SceneManagement;
 public class fullBandManager : MonoBehaviour
 {
     public GameManagerE2 gameManagerE2;
-    public GameObject error, error1, error2, error3, error4, error5, error6, error7, error8, error9, error10;
-    public int err;
 
     void Start()
     {
-        error.SetActive(false);
-        error1.SetActive(false);
-        error2.SetActive(false);
-        error3.SetActive(false);
-        error4.SetActive(false);
-        error5.SetActive(false);
-        error6.SetActive(false);
-        error7.SetActive(false);
-        error8.SetActive(false);
-        error9.SetActive(false);
-        error10.SetActive(false);
         gameManagerE2 = GameObject.FindGameObjectWithTag("GameManagerE2").GetComponent<GameManagerE2>();
     }
 
@@ -42,16 +29,6 @@ public class fullBandManager : MonoBehaviour
                 {
                     gameManagerE2.bandSpeed = 0;
                     StartCoroutine(ExampleCoroutine());
-                    //  error.SetActive(true);
-                    //   error1.SetActive(true);
-                    //    error2.SetActive(true);
-                    //     error3.SetActive(true);
-                    //      error4.SetActive(true);
-                    //       error5.SetActive(true);
-
-                    //        SceneManager.LoadScene("Ecran Défaite");
-
-                    //     Cursor.SetCursor(gameManagerE2.cursorTexture2, gameManagerE2.hotSpot, gameManagerE2.cursorMode);
                 }
             }
 
@@ -71,22 +48,11 @@ public class fullBandManager : MonoBehaviour
 
     IEnumerator ExampleCoroutine()
     {
-        error.SetActive(true);
-        error1.SetActive(true);
-        error2.SetActive(true);
-        error3.SetActive(true);
-        error4.SetActive(true);
-        error5.SetActive(true);
-        error6.SetActive(true);
-        error7.SetActive(true);
-        error8.SetActive(true);
-        error9.SetActive(true);
-        error10.SetActive(true);
-        yield return new WaitForSeconds(5);
-
-        SceneManager.LoadScene("Ecran Défaite");
-
         Cursor.SetCursor(gameManagerE2.cursorTexture2, gameManagerE2.hotSpot, gameManagerE2.cursorMode);
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("error scene");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("Ecran Défaite");
     }
 
 }
